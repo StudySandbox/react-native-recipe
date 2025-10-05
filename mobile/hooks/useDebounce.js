a68b1ebe15fdf7a0f81e09a2d8394a8f8cea7259
insu <hinsu93@gmail.com>
2025-10-05 18:04:20 +0900

    [FE] 검색 페이지 스타일 추가

---
+++ mobile/hooks/useDebounce.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+export function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => clearInterval(handler);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
